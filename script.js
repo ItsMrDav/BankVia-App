1790ff81c8ff5e9e88d72f43778246815c5578ea
Davut Simsek <davutt.simsek@gmail.com>
2025-01-26 17:36:37 +0100

    implement user login

--- script.js
+++ script.js
@@ -66,32 +66,7 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////// DISPLAY MOVEMENTS FUNCTIONALITY
-const displayMovements = function (movements) {
-  // Using, innerHTML DOM>Element method, setting all HTML content
-  containerMovements.innerHTML = ``;
-
-  // Using with forEach method, according to account movements, adding a html movement row
-  movements.forEach((mov, i) => {
-    const type = mov > 0 ? `deposit` : `withdrawal`;
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${
-      i + 1
-    }. ${type}</div>
-      <div class="movements__value">${mov}€</div>
-    </div>`;
-
-    // Using, insertAdjacentHTML DOM>Element method, adding HTML content to parent
-    // Choosing `afterbegin` option adds any new row after the last one
-    containerMovements.insertAdjacentHTML(`afterbegin`, html);
-  });
-};
-displayMovements(account1.movements);
-
-/////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////// COMPUTING USER NAMES FUNCTIONALITY
+//////////////////////////////////////////////////// IMPLEMENT USER NAME CREATION
 const createUserNames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -104,32 +79,83 @@ const createUserNames = function (accs) {
 createUserNames(accounts);
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////// CALC, DISPLAYING BALANCE FUNCTIONALITY
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
-};
-calcDisplayBalance(account1.movements);
+////////////////////////////////////////////////////// IMPLEMENT DISPLAY MOVEMENTS
+const displayMovements = function (movements) {
+  // Using, innerHTML DOM>Element method, setting all HTML content
+  containerMovements.innerHTML = ``;
 
+  // Using with forEach method, according to account movements, adding a html movement row
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    }. ${type}</div>
+      <div class="movements__value">${mov} €</div>
+    </div>`;
+
+    // Using, insertAdjacentHTML DOM>Element method, adding HTML content to parent
+    // Choosing `afterbegin` option adds any new row after the last one
+    containerMovements.insertAdjacentHTML(`afterbegin`, html);
+  });
+};
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////// CALC, DISPLAYING SUMMARY FUNCTIONALITY
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+//////////////////////////////////////////////////// IMPLEMENT CALCULATING and DISPLAYING BALANCE
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// IMPLEMENT CALCULATING DISPLAYING MOVEMENTS SUMMARY
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes} €`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out)} €`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => deposit * (1.2 / 100))
+    .map(deposit => deposit * (acc.interestRate / 100))
     .filter(int => int >= 1)
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest} €`;
 };
-calcDisplaySummary(account1.movements);
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// IMPLEMENT USER LOGIN
+// Login Button Event Handler
+let currentAccount;
+btnLogin.addEventListener(`click`, function (e) {
+  // since its a form submit button, this prevents default(form from submitting)
+  e.preventDefault();
+  // if user matches with any account's username define current user
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+  // check if pin is equal to current user's pin
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    labelWelcome.textContent = `Good Afternoon, ${
+      currentAccount.owner.split(` `)[0]
+    }!`;
+    containerApp.style.opacity = 1;
+    // Clear user and pin input fields
+    inputLoginUsername.value = inputLoginPin.value = ``;
+    inputLoginPin.blur();
+    // Display movements
+    displayMovements(currentAccount.movements);
+    // Calculate, display balance
+    calcDisplayBalance(currentAccount.movements);
+    // Calculate, display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
