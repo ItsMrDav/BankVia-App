9ee0e87cda3f3f3119632ab5a044f2d8ede72f32
Davut Simsek <davutt.simsek@gmail.com>
2025-01-29 16:54:17 +0100

    refactor number's display

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ const displayMovements = function (movements, sort = false) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     }. ${type}</div>
-      <div class="movements__value">${mov} €</div>
+      <div class="movements__value">${mov.toFixed(2)} €</div>
     </div>`;
 
     // Using, insertAdjacentHTML DOM>Element method, adding HTML content to parent
@@ -164,7 +164,7 @@ const displayMovements = function (movements, sort = false) {
 //////////////////////////////////////////////////// IMPLEMENT CALCULATING and DISPLAYING BALANCE
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance} €`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} €`;
 };
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
@@ -173,19 +173,19 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes} €`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)} €`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => deposit * (acc.interestRate / 100))
     .filter(int => int >= 1)
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest} €`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
@@ -246,7 +246,7 @@ btnTransfer.addEventListener(`click`, function (e) {
 btnLoan.addEventListener(`click`, function (e) {
   e.preventDefault();
   // Store loan amount
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
   // Check condition for loan
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add loan movement
@@ -288,3 +288,229 @@ btnSort.addEventListener(`click`, function (e) {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// ARRAY GROPING(ES2024)
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// console.log(movements);
+
+// const groupedMovements = Object.groupBy(movements, mov =>
+//   mov > 0 ? `deposits` : `withdrawal`
+// );
+// console.log(groupedMovements);
+// console.log(typeof groupedMovements);
+
+// const groupedByActivity = Object.groupBy(accounts, account => {
+//   const movementCount = account.movements.length;
+//   if (movementCount >= 8) return `very active`;
+//   if (movementCount >= 4) return `active`;
+//   if (movementCount >= 1) return `moderate`;
+//   return `inactive`;
+// });
+// console.log(groupedByActivity);
+
+// const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
+// console.log(groupedAccounts);
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// MORE WAYS CREATING AND FILLING ARRAYS
+// // Empty arrays + fill method
+// const arr = [1, 2, 3, 4, 5];
+// console.log(new Array(1, 2, 3, 4, 5));
+
+// const x = new Array(7);
+// x.fill(1, 3, 5);
+// console.log(x);
+// console.log(arr.fill(23, 2, 4));
+// // Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y);
+
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z);
+
+// const aa = Array.from({ length: 10 }, () => Math.ceil(Math.random() * 100));
+// console.log(aa);
+
+// // Array from can be worked with DOM elements
+// labelBalance.addEventListener(`click`, function (e) {
+//   e.preventDefault();
+//   const movementsUI = Array.from(
+//     document.querySelectorAll(`.movements__value`),
+//     el => Number(el.textContent.replace(`€`, ``))
+//   );
+//   console.log(movementsUI);
+
+//   // Same thing with Array.from static method can be happen with spread operator
+//   const movementsUI2 = [...document.querySelectorAll(`.movements__value`)].map(
+//     el => Number(el.textContent.replace(`€`, ``))
+//   );
+//   console.log(movementsUI2);
+// });
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/////////////////////// NON-DESTRUCTIVE ALTERNATIVES: toSorted, toReversed, toSpliced, with
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// console.log(movements);
+// // toReversed
+// const reversedMov = movements.toReversed();
+// console.log(reversedMov);
+
+// // with
+// const newMovements = movements.with(1, 2000);
+// console.log(newMovements);
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// SUMMARY OF ARRAY METHODS
+// // 1.
+// const bankDepositSum = accounts
+//   .flatMap(mov => mov.movements)
+//   .filter(mov => mov > 0)
+//   .reduce((sum, cur) => sum + cur);
+// console.log(bankDepositSum);
+// // 2.
+// // const numDeposits1000 = accounts
+// //   .flatMap(mov => mov.movements)
+// //   .filter(mov => mov >= 1000).length;
+// const numDeposits1000 = accounts
+//   .flatMap(mov => mov.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+// console.log(numDeposits1000);
+
+// // Prefixed a ++ operator
+// let a = 10;
+// console.log(++a);
+// console.log(a);
+// // 3.
+// const { deposits, withdrawals } = accounts
+//   .flatMap(mov => mov.movements)
+//   .reduce(
+//     (sums, mov) => {
+//       sums[mov > 0 ? `deposits` : `withdrawals`] += mov;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+// console.log(deposits, withdrawals);
+// // 3.1.
+// const bankDepositSum1 = accounts.reduce((sum, acc) => {
+//   return (
+//     sum + acc.movements.reduce((acc, mov) => (mov > 0 ? acc + mov : acc), 0)
+//   );
+// }, 0);
+// console.log(bankDepositSum1);
+// // 3.2.
+// const numDeposits10001 = accounts.reduce((count, cur) => {
+//   return (
+//     count +
+//     cur.movements.reduce((count, cur) => (cur >= 1000 ? ++count : count), 0)
+//   );
+// }, 0);
+// console.log(numDeposits10001);
+// // 3.3.
+// const { deposits1, withdrawals1 } = accounts.reduce(
+//   (sums, acc) => {
+//     return acc.movements.reduce((accSum, mov) => {
+//       accSum[mov > 0 ? 'deposits1' : 'withdrawals1'] += mov;
+//       return accSum;
+//     }, sums);
+//   },
+//   { deposits1: 0, withdrawals1: 0 }
+// );
+// console.log(deposits1, withdrawals1);
+// 4.
+// const convertTitleCase = function (title) {
+//   const capitilaze = str => str[0].toUpperCase() + str.slice(1);
+
+//   const exceptions = [`a`, `an`, `and`, `the`, `but`, `or`, `on`, `in`, `with`];
+//   const titleCase = title
+//     .trim()
+//     .toLowerCase()
+//     .split(` `)
+//     .map(word => (exceptions.includes(word) ? word : capitilaze(word)))
+//     .join(` `);
+//   return capitilaze(titleCase);
+// };
+
+// console.log(convertTitleCase(` this is a nice title`));
+// console.log(convertTitleCase(`    this is a LONG title but not too long`));
+// console.log(convertTitleCase(` and here is another title with an EXAMPLE   `));
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// NUMBERS
+// console.log(23 === 23.0);
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
+
+// // Conversion
+// console.log(typeof Number(`23`));
+// console.log(typeof +`23`);
+
+// // Parsing (string has to be start with number otherwise wouldnt work)
+// console.log(Number.parseInt(`30px`, 10));
+// console.log(Number.parseInt(`e23`, 10));
+
+// console.log(Number.parseInt(`  2.5rem`));
+// // parseFloat is better way to get a number from a string
+// console.log(Number.parseFloat(` 2.5rem`));
+
+// // isNaN (Check if value is NaN)
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
+
+// // isFinite (BEST WAY to check if a value is a number)
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite(`20`));
+// console.log(Number.isFinite(+`20X`));
+// console.log(Number.isFinite(23 / 0));
+
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23 / 0));
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////// MATH
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, `23`, 11, 2));
+// console.log(Math.max(5, 18, `23px`, 11, 2));
+
+// console.log(Math.min(5, 18, 23, 11, 2));
+
+// console.log(Math.PI * Number.parseFloat(`10px`) ** 2);
+
+// console.log(Math.ceil(Math.random() * 6));
+
+// const randomInt = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
+// console.log(randomInt(10, 20));
+// console.log(randomInt(0, 3));
+
+// Rounding Integers
+// console.log(Math.trunc(23.3));
+
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(`23.9`));
+
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
+
+// Rounding Integers
+// console.log((2.7).toFixed());
+// console.log((2.7).toFixed(3));
+// console.log((2.7212312).toFixed(3));
+// console.log((2).toPrecision(3));
+
+// console.log((3).toExponential(3));
